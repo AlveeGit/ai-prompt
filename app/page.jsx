d3aa21c61b1d88a4a30e0fbca31b080e9e57f40c
AlveeGit <alveekabir1@gmail.com>
2023-06-08 08:51:58 +0600

    Responsive Navigation created

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,20 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="min-md:hidden" />
+        <span className="text-center orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="text-center desc">
+        AI-Prompt is a platform for AI-powered prompts. Share your prompts with friends, family, and the world. 
+      </p>
+      
+      <Feed />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
