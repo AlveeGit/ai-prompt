e4b12d864fd1db6b87143bc149ac0117cf95d17e
AlveeGit <alveekabir1@gmail.com>
2023-06-08 11:18:53 +0600

    next auth on progress

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,28 @@
-import '@styles/globals.css';
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Ai-Prompt",
-    description: 'Discover and share AI prompts'
-}
+  title: "Ai-Prompt",
+  description: "Discover and share AI prompts",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
-      <html lang='en'>
-          <body>
-              <div className='main'>
-                  <div className='gradient' />
-              </div>
-              <main className='app'>
-                  <Nav />
-                  {children}
-              </main>
-          </body>
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
---
+++ utils/database.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set("strictQuery", true);
+    if(!isConnected) {
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
