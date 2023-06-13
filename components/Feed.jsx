ee1fa814998403a63ab2e7be50b52f63739d8f05
AlveeGit <alveekabir1@gmail.com>
2023-06-13 08:10:15 +0600

    navbar, login basic created

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,28 @@
-const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+"use client";
+import { useEffect, useState } from "react";
 
-export default Feed
+const PromptCardList = ({ data, handleTagClick }) => {
+  return <div className="mt-16 prompt_layout"></div>;
+};
+const Feed = () => {
+  const [searchText, setsearchText] = useState("");
+  const handleSearchChange = (e) => {};
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a user name"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={[]} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
--- utils/database.js
+++ utils/database.js
@@ -3,17 +3,21 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const connectToDB = async () => {
-    mongoose.set("strictQuery", true);
-    if(!isConnected) {
-        return;
-    }
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("Database already connected");
+    return;
+  }
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+  }
 };
